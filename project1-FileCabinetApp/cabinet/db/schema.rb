0074023f8024243fc460d20c64b547432579844b
pauloskaterock <pauloengenharia5@yahoo.com>
2025-09-10 21:00:58 -0300

    StyleScheets

--- project1-FileCabinetApp/cabinet/db/schema.rb
+++ project1-FileCabinetApp/cabinet/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_10_194819) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_10_213810) do
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "context"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,4 +30,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_10_194819) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "docs", "users"
 end
