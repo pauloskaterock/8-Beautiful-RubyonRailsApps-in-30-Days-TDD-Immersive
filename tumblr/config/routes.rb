8967fccc0aa280244e5571ac0cd3238b6f063fb4
pauloskaterock <pauloengenharia5@yahoo.com>
2025-09-18 14:47:14 -0300

    Tumblr Static Page

--- tumblr/config/routes.rb
+++ tumblr/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # get "up" => "rails/health#show", as: :rails_health_check
-  resources :posts
-  resources :comments
+    # get "up" => "rails/health#show", as: :rails_health_check
+    resources :posts do
+    resources :comments
+    end
   root "posts#index"
+
+  get "/about", to: "pages#about"
 end
